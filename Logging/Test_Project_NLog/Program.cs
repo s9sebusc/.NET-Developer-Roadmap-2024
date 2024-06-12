6e31efc0fb2752ee99e1dcdcc5f0b7c68236c209
Sergej Busch <sergej.busch@zeiss.com>
2024-06-12 15:42:24 +0200

    [Logging] Added test project for NLog

---
+++ Logging/Test_Project_NLog/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using NLog;
+
+Logger log = LogManager.GetCurrentClassLogger();
+log.Info("This is a debug level message");
+
+Logger consoleLogger = LogManager.GetLogger("MyConsoleLogger");
+consoleLogger.Debug("Test log writte by console logger");
+
+Logger logFileLogger = LogManager.GetLogger("MyLogfileLogger");
+logFileLogger.Info("Test log written by log file logger");
+logFileLogger.Error("Error log written by log file logger");
+
+Console.ReadKey();

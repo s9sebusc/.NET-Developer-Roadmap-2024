f2f56bf10302a7910b4df0cd0b17abad378821f7
Sergej Busch <sergej.busch@zeiss.com>
2024-06-12 15:42:36 +0200

    [Logging] Added test project for SeriLog

---
+++ Logging/Test_Project_SeriLog/DummyLoggerEnricher.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Serilog.Core;
+using Serilog.Events;
+
+namespace Test_Project_SeriLog
+{
+	public class DummyLoggerEnricher : ILogEventEnricher
+	{
+		private readonly string _environment;
+
+		public DummyLoggerEnricher(string environment)
+		{
+			_environment = environment;
+		}
+
+		public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+		{
+			LogEventProperty environmentProperty = propertyFactory.CreateProperty("Environment", _environment);
+			logEvent.AddPropertyIfAbsent(environmentProperty);
+		}
+	}
+}
---
+++ Logging/Test_Project_SeriLog/Program.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.Extensions.Configuration;
+using Serilog.Settings.Configuration;
+using Serilog;
+using Serilog.Core;
+using Test_Project_SeriLog;
+using System.Diagnostics;
+
+Serilog.Debugging.SelfLog.Enable(x => Debug.WriteLine(x)); // Enable self-logging
+
+Logger fileLogger = new LoggerConfiguration()
+	.MinimumLevel.Debug()
+	.WriteTo.File(@"D:\serilog.log")
+	.CreateLogger();
+
+fileLogger.Debug("This is a test log by Serilog "); // Default Level is already INFO!
+fileLogger.Debug($"Today is {DateTime.Now.Date}");
+
+
+string defaultOutputTemplate = "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}";
+Logger consoleLogger = new LoggerConfiguration()
+	.MinimumLevel.Debug()
+	.WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3} Version:{Version}] {Message:lj}{NewLine}{Exception}") // Version added as part of message
+	.Enrich.WithProperty("Version", "1.2.3") // Add custom properties
+	.CreateLogger();
+	
+consoleLogger.Debug("Test console log");
+
+Logger loggerConfiguredByJsonFile = new LoggerConfiguration().ReadFrom.Configuration(UseJsonConfiguration()).CreateLogger();
+loggerConfiguredByJsonFile.Information("Test log for json configuration");
+
+//Logger loggerConfiguredByIniFile = new LoggerConfiguration()
+//	.ReadFrom.Configuration(UseIniConfiguration())
+//	.CreateLogger();
+//loggerConfiguredByIniFile.Information("Test log for ini configuration");
+
+
+//Logger loggerConfiguredByYamlFile = new LoggerConfiguration()
+//	.ReadFrom.Configuration(UseYamlConfiguration())
+//	.CreateLogger();
+//loggerConfiguredByYamlFile.Debug("Test log for yaml configuration");
+
+Logger loggerConfiguredByXmlFile = new LoggerConfiguration().ReadFrom.Configuration(UseXmlConfiguration()).CreateLogger();
+loggerConfiguredByXmlFile.Information("Test log for xml configuration"); // No logs :((
+
+Log.CloseAndFlush();
+
+
+//// Configuraton by config file
+static IConfigurationRoot UseJsonConfiguration()
+{
+	IConfigurationRoot configuration = new ConfigurationBuilder()
+		.SetBasePath(Directory.GetCurrentDirectory())
+		.AddJsonFile("serilog_configuration.json")
+		.Build();
+
+	return configuration;
+}
+
+static IConfigurationRoot UseIniConfiguration()
+{
+	IConfigurationRoot configuration = new ConfigurationBuilder()
+		.AddIniFile("serilog_configuration.ini")
+		.Build();
+
+	return configuration;
+}
+
+static IConfigurationRoot UseYamlConfiguration()
+{
+	IConfigurationRoot configuration = new ConfigurationBuilder()
+		.SetBasePath(Directory.GetCurrentDirectory())
+		.AddYamlFile("serilog_configuration.yaml")
+		.Build();
+
+	return configuration;
+}
+
+static IConfigurationRoot UseXmlConfiguration()
+{
+	IConfigurationRoot configuration = new ConfigurationBuilder()
+		.SetBasePath(Directory.GetCurrentDirectory())
+		.AddXmlFile("serilog_configuration.xml")
+		.Build();
+
+	return configuration;
+}

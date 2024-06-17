ef4b8a13aac926fd11e0b7d89f05df49e9b13c21
Sergej Busch <sergej.busch@zeiss.com>
2024-06-17 11:53:05 +0200

    [General Skills] Added prototype design pattern

---
+++ General Skills/Design Patterns/Prototype/Program.cs
@@ -0,0 +1,24 @@
+﻿using Prototype;
+
+Sheep sheep_original = new Sheep() { Age = 10, Name = "My Sheep" };
+
+// The clone class has all values of the original class. Also the private values.
+Sheep sheep_clone = (Sheep)sheep_original.Clone(); 
+
+Console.WriteLine("Original name: " + sheep_original.Name); // My Sheep
+Console.WriteLine("Original age: " + sheep_original.Age); // 10
+
+Console.WriteLine("Clone name: " + sheep_clone.Name); // My Sheep
+Console.WriteLine("Clone age: " + sheep_clone.Age); // 10
+
+sheep_clone.Name = "Dolly";
+sheep_clone.Age = 1;
+
+Console.WriteLine("Original name: " + sheep_original.Name); // My Sheep
+Console.WriteLine("Original age: " + sheep_original.Age); // 10
+
+// The copy of the sheep is a deep copy!
+Console.WriteLine("Clone name: " + sheep_clone.Name); // Dolly
+Console.WriteLine("Clone age: " + sheep_clone.Age); // 1
+
+Console.ReadKey();
---
+++ General Skills/Design Patterns/Prototype/Sheep.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+	internal class Sheep : ICloneable // das Interface ist in .NET 8 bereits integriert
+	{
+		private string anInternalVariable;
+
+		public string? Name { get; set; }
+		public int Age { get; set; }
+
+		public Sheep()
+		{
+			this.anInternalVariable = "This value isn't visible from outside";	
+		}
+
+		public object Clone()
+		{
+			return (Sheep)this.MemberwiseClone();
+		}
+	}
+}
